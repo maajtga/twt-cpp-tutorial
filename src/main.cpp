2ead5d322f034a41222ecb9f5fe9f52ea3765ee8
maajtga <maajtga@users.noreply.github.com>
2021-03-07 21:18:14 -0500

    Create firstheader.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "firstheader.h"
 
 using namespace std;
 
 int main() { 
+  
+    // Given two numbers 
+    int a = 13, b = 22; 
+  
+    // Function declared in header 
+    // file to find the sum 
+    cout << "Sum is: "
+         << sumOfTwoNumbers(a, b);         
 
-	cout << "Hello World";
-	
-	return 0;
 }
